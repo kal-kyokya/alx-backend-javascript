dd001d0340d40dec55ee19d3bde2ffa96a817948
kal-kyokya <kalkyokya4@gmail.com>
2024-10-01 04:48:59 -0700

    Condensate lines of code into one using default parameters.

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+export default function getSumOfHoods(initialNumber,
+				      expansion1999 = 25,
+				      expansion2003 = 21) {
+    return initialNumber + expansion1999 + expansion2003;
+}
---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+import getSumOfHoods from './3-default-parameter';
+
+console.log(getSumOfHoods(28, 25, 21));
+console.log(getSumOfHoods(28, 25));
+console.log(getSumOfHoods(28));
