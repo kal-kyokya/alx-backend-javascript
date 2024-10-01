cbc138a8003e6252d92d303747f61bde1778396f
kal-kyokya <kalkyokya4@gmail.com>
2024-10-01 05:00:56 -0700

    Resolve elint errors due to wrong identation of function parameters when each on its own line.

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,7 @@
-export default function getSumOfHoods(initialNumber,
-				      expansion1999 = 25,
-				      expansion2003 = 21) {
-    return initialNumber + expansion1999 + expansion2003;
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1999 = 25,
+  expansion2003 = 21,
+) {
+  return initialNumber + expansion1999 + expansion2003;
 }
