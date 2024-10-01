aab4db243024d747342b4bc896008c3649a759f7
kal-kyokya <kalkyokya4@gmail.com>
2024-10-01 06:23:35 -0700

    Utilize rest parameter to count input args of a function.

--- 0x00-ES6_basic/4-main.js
+++ 0x00-ES6_basic/4-main.js
@@ -1,4 +1,4 @@
 import returnHowManyArguments from './4-rest-parameter';
 
-console.log(returnHowManyArguments("Dem", 25, "Allah"));
+console.log(returnHowManyArguments('Dem', 25, 'Allah'));
 console.log(returnHowManyArguments());
