fc79521ebd9c7f7e40c665f1562e64188b5e92a1
kal-kyokya <kalkyokya4@gmail.com>
2024-10-02 04:15:53 -0700

    Make use of computed property names to dynamically initialize an object.

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+function getcurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    ['income' + getcurrentYear()] = income;
+    ['gdp'  + getcurrentYear()] = gdp;
+    ['capita'  + getcurrentYear()] = capita;
+  };
+}
