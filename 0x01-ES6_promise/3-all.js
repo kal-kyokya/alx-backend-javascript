7d690f7603158035eccff2dd939b279196f4849a
kal-kyokya <kalkyokya4@gmail.com>
2024-10-04 11:35:10 -0700

    Chain promises using 'then...catch', try 3.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,23 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let str = '';
-  uploadPhoto()
-    .then((photo) => {
-      str += `${photo.body} `;
-      return createUser();
-    })
-    .then((profile) => {
-      str += `${profile.firstName} `;
-      str += profile.lastName;
+  return new Promise((resolve, reject) => {
+    let str = '';
+    uploadPhoto()
+      .then((photo) => {
+        str += `${photo.body} `;
+        return createUser();
+      })
+      .then((profile) => {
+        str += `${profile.firstName} `;
+        str += profile.lastName;
 
-      console.log(str);
-    })
-    .catch(() => console.log('Signup system offline'));
+        console.log(str);
+        resolve(str);
+      })
+      .catch(() => {
+        console.log('Signup system offline');
+        reject(Error());
+      });
+  });
 }
