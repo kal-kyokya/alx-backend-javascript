cae101fcceb87571c3c8d24d6cdfea677df9ba0e
kal-kyokya <kalkyokya4@gmail.com>
2024-10-05 15:03:09 -0700

    Experiment with class static methods.

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -13,7 +13,7 @@ export default class Pricing {
     this._currency = currency;
   }
 
-  get amount() { return this.amount; }
+  get amount() { return this._amount; }
 
   set amount(amount) {
     if (typeof amount !== 'number') throw new TypeError('amount must be a number');
