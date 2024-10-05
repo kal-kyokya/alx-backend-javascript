e6f12dbb268d8d2fef8fe8db380f479a18a2cde8
kal-kyokya <kalkyokya4@gmail.com>
2024-10-05 15:01:59 -0700

    Experiment with class static methods.

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -30,7 +30,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
