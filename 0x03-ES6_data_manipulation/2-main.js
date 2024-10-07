f004d7ad278cca647865f075d9a1e0071284856f
kal-kyokya <kalkyokya4@gmail.com>
2024-10-07 15:24:44 -0700

    Utilize the 'filter' method to process a list of objects.

---
+++ 0x03-ES6_data_manipulation/2-main.js
@@ -0,0 +1,4 @@
+import getListStudents from './0-get_list_students';
+import getStudentsByLocation from './2-get_students_by_loc';
+
+console.log(getStudentsByLocation(getListStudents(), 'San Francisco'));
