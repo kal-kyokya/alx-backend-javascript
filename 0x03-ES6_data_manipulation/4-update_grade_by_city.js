8b6619292a6d5d6833eada3f6de1e3b00a293c0e
kal-kyokya <kalkyokya4@gmail.com>
2024-10-08 04:06:56 -0700

    Create a typed array and return the data view associated with it.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,13 @@
 export default function updateStudentGradeByCity(studentsList, city, grades) {
-    return studentsList
-        .filter((student) => student.location === city)
-        .map((student) => {
-            // Find the grade object with matching studentId
-            const gradeObj = grades.find((grade) => grade.studentId === student.id);
-            
-            // If a matching grade object exists, return updated student with the grade
-            if (gradeObj) {
-                return { ...student, grade: gradeObj.grade };
-            } else {
-                return { ...student, grade: 'N/A' };  // Assign 'N/A' if no grade found
-            }
-        });
+  return studentsList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      // Find the grade object with matching studentId
+      const gradeObj = grades.find((grade) => grade.studentId === student.id);
+
+      // If a matching grade object exists, return updated student with the grade
+      if (gradeObj) return { ...student, grade: gradeObj.grade };
+      // Assign 'N/A' if no grade found
+      return { ...student, grade: 'N/A' };
+    });
 }
