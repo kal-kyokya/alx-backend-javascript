41e950e771dcf1fd4bdcb539a24e2dce44737036
kal-kyokya <kalkyokya4@gmail.com>
2024-10-09 04:29:57 -0700

    Create a typed array and return the data view associated with it.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) throw new Error('Position outside range');
+  if (position < 0 && position >= length) throw new Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const dataView = new DataView(buffer, 0, length);
-  const int8view = new Int8Array(length);
+  const int8view = new Int8Array(buffer);
   int8view[position] = value;
 
   return (dataView);
