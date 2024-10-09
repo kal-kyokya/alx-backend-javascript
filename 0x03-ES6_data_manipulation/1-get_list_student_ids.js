a357a221795cc07de712957401c8f4f700f51966
kal-kyokya <kalkyokya4@gmail.com>
2024-10-09 03:00:21 -0700

    Make use of the 'map' method to customize the return value of a function.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(objArray) {
-  if (typeof objArray !== 'object') return [];
+  if (!(objArray instanceof Array)) return [];
   const idList = objArray.map((student) => student.id);
 
   return idList;
--- 0x03-ES6_data_manipulation/1-main.js
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -2,4 +2,6 @@ import getListStudents from './0-get_list_students';
 import getListStudentIds from './1-get_list_student_ids';
 
 console.log(getListStudentIds(getListStudents()));
-console.log(getListStudentIds('Dj Dem'));
+console.log(getListStudentIds(getListStudents()).length);
+console.log(getListStudentIds(null));
+console.log(getListStudentIds('dem'));
