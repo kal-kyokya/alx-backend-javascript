b93d6ac8758f6860dc8a5c176f4a9d71bc958f5e
kal-kyokya <kalkyokya4@gmail.com>
2024-10-09 04:01:47 -0700

    Manipulate a set's string element and return a string.

--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,7 @@
 import cleanSet from './8-clean_set';
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', '', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(cleanSet(new Set(['banjovi', 'banaparte', 'banappetit', null, 'banana']), 'ba'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), []));
+console.log(cleanSet(new Set(['banjovi', 'banaparte', 'banappetit', 8, 'banana']), 'ba'));
