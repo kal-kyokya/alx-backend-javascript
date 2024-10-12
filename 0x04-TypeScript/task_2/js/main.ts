b35cf75d717c55cd45e4fcaf9545deebfa14d6ef
kal-kyokya <kalkyokya4@gmail.com>
2024-10-12 12:49:24 -0700

    Create a string literal type using the 'type' keyword.

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -84,3 +84,15 @@ console.log(executeWork(createEmployee(200)));
 //Getting to work
 console.log(executeWork(createEmployee(1000)));
 //Getting to director tasks
+
+type Subjects = ('Math', 'History');
+
+function teachClass(todayClass): (Math | History) {
+  if (todayClass === 'Math') return 'Teaching Math';
+  if (todayClass === 'History') return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+//Teaching Math
+console.log(teachClass('History'));
+//Teaching History
