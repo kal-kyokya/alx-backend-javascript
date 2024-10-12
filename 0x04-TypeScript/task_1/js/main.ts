d44bb5e1c9af10fd6b57a88751907870ceadedf1
kal-kyokya <kalkyokya4@gmail.com>
2024-10-12 02:01:20 -0700

    Create an object interface extension.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,31 @@ location: string;
 [key: string]: any
 }
 
-const teacher3: Teacher = {
+const teacher: Teacher = {
   firstName: 'Jean-Paul',
   fullTimeEmployee: false,
   lastName: 'Kyokya',
   location: 'Kinshasa',
   course: 'football',
 };
+console.log(teacher);
 
-console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: 25
+}
+
+const teacher1: Teacher = {
+  firstName: 'Jean-Paul',
+  fullTimeEmployee: false,
+  lastName: 'Kyokya',
+  location: 'Kinshasa',
+  course: 'football',
+  numberOfReports: 17,
+};
+console.log(teacher1);
+
+interface printTeacher(firstName: string, lastName: string): string {
+  return (`${firstName[]}. ${lastName}`);
+}
+
+console.log(printTeacher('Jean-Paul', 'Kyokya'));
