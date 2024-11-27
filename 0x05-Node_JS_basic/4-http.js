45bde52394474e74ec24484d2564f3530e719c8f
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 02:49:07 -0800

    Create a small http server using the 'http' module.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,10 +2,15 @@
 
 const http = require('http');
 
+const PORT = 1245;
+const HOST = 'localhost';
+
 app = http.createServer((req, res) => {
     res.end('Hello Holberton School !');
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${PORT}:${PORT}\n`);
+});
 
 module.exports = app;
