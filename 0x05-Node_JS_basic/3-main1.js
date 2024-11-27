69ebd53fbe966c77e62f7a6565edeb73fdc2a41a
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 03:17:00 -0800

    Create a more complex http server using the 'http' module.

--- 0x05-Node_JS_basic/3-main1.js
+++ 0x05-Node_JS_basic/3-main1.js
@@ -1,12 +1,12 @@
 const countStudents = require('./3-read_file_async');
 
 countStudents('database.csv')
-    .then(() => {
-        console.log('Done!');
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+  .then(() => {
+    console.log('Done!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 console.log('Do some of this !\n');
 
