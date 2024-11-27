c321fa2bf822e18a8be6332b85999c0da5267960
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 03:39:37 -0800

    Organize a complex HTTP server using Express.

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,20 +8,13 @@ const HOST = 'localhost';
 const app = http.createServer();
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  if (!dataPath) {
-    reject(new Error('Cannot load the database'));
-  }
+  if (!dataPath) reject(new Error('Cannot load the database'));
+
   if (dataPath) {
     fs.readFile(dataPath, (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
+      if (err) reject(new Error('Cannot load the database'));
+
       if (data) {
         const reportParts = [];
         const fileLines = data.toString('utf-8').trim().split('\n');
---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+// This script writes the App controller
+
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
