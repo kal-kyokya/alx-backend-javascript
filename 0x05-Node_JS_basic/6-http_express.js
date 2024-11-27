a96b4c62dc9bac151115ce0fb8a8f2c62e7a4699
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 03:05:38 -0800

    Create a small http server using the 'express' module.

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,13 +3,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = 1245;
 
-app.get('/', (req, res) => {
-  res.end('Hello Holberton School !');
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-    console.log('Server listening on PORT 1245');
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
