3b10f2d788ff855b94fdfc64bc03c8e49a9b1ebe
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 02:36:27 -0800

    Generate a database report asynchronously.

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,45 @@
-// This script attempts to read a database file asynchronously
+// This script attempts to read a database file synchronously
 
-const { readFile } = require('fs');
+const fs = require('fs');
 
-module.exports = function countStudents(database) {
-  return new Promise((resolve, reject) => {
-    readFile(database, (err, data) => {
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) reject(new Error('Cannot load the database'));
 
-        if (err) reject(new Error('Cannot load the database'));
+    if (data) {
+      const fileLines = data
+        .toString('utf-8')
+        .trim()
+        .split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames
+        .slice(0, dbFieldNames.length - 1);
 
-        const records = data.toString().split('\n');
-        console.log(`Number of students: ${records.length - 2}`);
-
-        const students = { CS: [], SWE: [] };
-
-        for (let i = 1; i < records.length - 1; i += 1) {
-            const studentFields = records[i].split(',');
-            if (studentFields[studentFields.length - 1] === 'CS') {
-                students['CS'].push(studentFields[0]);
-            }
-            if (studentFields[studentFields.length - 1] === 'SWE') {
-                students['SWE'].push(studentFields[0]);
-            }
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord
+          .slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
         }
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
+      }
 
-        csList = students.CS.join(', ');
-        sweList = students.SWE.join(', ');
-        console.log(`Number of students in CS: ${students.CS.length}. List: ${csList}`);
-        console.log(`Number of students in SWE: ${students.SWE.length}. List: ${sweList}`);
-
-        resolve(true);
-    });
+      const totalStudents = Object
+        .values(studentGroups)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+      }
+      resolve(true);
+    }
   });
-};
+});
+
+module.exports = countStudents;
