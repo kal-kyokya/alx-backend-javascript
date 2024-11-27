5e4a7c4ffaedf1c91bcea70cd339d17b2e8d514b
kal-kyokya <kalkyokya4@gmail.com>
2024-11-27 00:54:15 -0800

    Generate a database report synchronously.

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,10 +1,9 @@
 // This script attempts to read a database file synchronously
 
-const { readFile } = require('fs');
+const fs = require('fs');
 
 module.exports = function countStudents(database) {
-  readFile(database, (err, data) => {
-
+  fs.readFile(database, (err, data) => {
     if (err) throw new Error('Cannot load the database');
 
     const records = data.toString().split('\n');
@@ -15,15 +14,15 @@ module.exports = function countStudents(database) {
     for (let i = 1; i < records.length - 1; i += 1) {
       const studentFields = records[i].split(',');
       if (studentFields[studentFields.length - 1] === 'CS') {
-        students['CS'].push(studentFields[0]);
+        students.CS.push(studentFields[0]);
       }
       if (studentFields[studentFields.length - 1] === 'SWE') {
-        students['SWE'].push(studentFields[0]);
+        students.SWE.push(studentFields[0]);
       }
     }
 
-    csList = students.CS.join(', ');
-    sweList = students.SWE.join(', ');
+    const csList = students.CS.join(', ');
+    const sweList = students.SWE.join(', ');
     console.log(`Number of students in CS: ${students.CS.length}. List: ${csList}`);
     console.log(`Number of students in SWE: ${students.SWE.length}. List: ${sweList}`);
   });
